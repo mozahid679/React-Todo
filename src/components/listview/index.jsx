2005cfefcee4cf6fe5055392bf2834f984888262
mozahid679 <mozahid679@gmail.com>
2022-03-21 15:47:44 +0600

    Tod o App with react

---
+++ src/components/listview/index.jsx
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import React from "react";
+import { Button, Input, ListGroup, ListGroupItem } from "reactstrap";
+
+//List Item Component
+const ListItem = ({ todo, toggleComplete, toggleSelect }) => {
+  return (
+    <ListGroupItem className="d-flex align-items-center">
+      <Input
+        type="checkbox"
+        id={todo.id}
+        checked={todo.isSelect}
+        onChange={() => toggleSelect(todo.id)}
+      />
+      <div className="mx-3">
+        <h4>{todo.text}</h4>
+        <p>{todo.time.toDateString()}</p>
+      </div>
+      <Button
+        className="ml-auto"
+        color={todo.isComplete ? "danger" : "success"}
+        onClick={() => toggleComplete(todo.id)}
+      >
+        {todo.isComplete ? "Completed" : "Running"}
+      </Button>
+    </ListGroupItem>
+  );
+};
+
+ListItem.propTypes = {
+  todo: PropTypes.object.isRequired,
+  toggleSelect: PropTypes.func.isRequired,
+  toggleComplete: PropTypes.func.isRequired,
+};
+
+const ListView = ({ todos, toggleSelect, toggleComplete }) => {
+  return (
+    <ListGroup>
+      {todos.map((todo) => (
+        <ListItem
+          key={todo.id}
+          todo={todo}
+          toggleSelect={toggleSelect}
+          toggleComplete={toggleComplete}
+        />
+      ))}
+    </ListGroup>
+  );
+};
+
+ListView.propTypes = {
+  todos: PropTypes.object.isRequired,
+  toggleSelect: PropTypes.func.isRequired,
+  toggleComplete: PropTypes.func.isRequired,
+};
+export default ListView;
